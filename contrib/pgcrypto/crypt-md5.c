4ec65b380e45505ca0b0ef629486f39f2bdda0ed
Daniel Gustafsson <daniel@yesql.se>
2025-02-05 17:51:13 +0100

    fix the const

--- contrib/pgcrypto/crypt-md5.c
+++ contrib/pgcrypto/crypt-md5.c
@@ -33,11 +33,11 @@ _crypt_to64(char *s, unsigned long v, int n)
 char *
 px_crypt_md5(const char *pw, const char *salt, char *passwd, unsigned dstlen)
 {
-	static char *magic = "$1$"; /* This string is magic for this algorithm.
-								 * Having it this way, we can get better later
-								 * on */
-	static char *p;
-	static const char *sp,
+	static const char *magic = "$1$";	/* This string is magic for this
+										 * algorithm. Having it this way, we
+										 * can get better later on */
+	char	   *p;
+	const char *sp,
 			   *ep;
 	unsigned char final[MD5_SIZE];
 	int			sl,

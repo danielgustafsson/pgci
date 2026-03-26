9191cf4abff5f570254bde0b742993021a3c3f49
Daniel Gustafsson <dgustafsson@postgresql.org>
2026-03-26 21:55:06 +0100

    Online enabling and disabling of data checksums
    This allows data checksums to be enabled, or disabled, in a running
    cluster without restricting access to the cluster during processing.
    
    Data checksums could prior to this only be enabled during initdb or
    when the cluster is offline using the pg_checksums app. This commit
    introduce functionality to enable, or disable, data checksums while
    the cluster is running regardless of how it was initialized.
    
    A background worker launcher process is responsible for launching a
    dynamic per-database background worker which will mark all buffers
    dirty for all relation with storage in order for them to have data
    checksums calcuated on write.  Once all relations in all databases
    have been processed, the data_checksums state will be set to on and
    the cluster will at that point be identical to one which had data
    checksums enabled during initialization or via offline processing.
    
    When data checksums are being enabled, concurrent I/O operations
    from backends other than the data checksums worker will write the
    checksums but not verify them on reading.  Only when all backends
    have absorbed the procsignalbarrier for setting data_checksums to
    on will they also start verifying checksums on reading.  The same
    process is repeated during disabling; all backends write checksums
    but do not verify them until the barrier for setting the state to
    off has been absorbed by all.  This in-progress state is used to
    ensure there are no false negatives (or positives) due to reading
    a checksum which is not in sync with the page.
    
    A new testmodule, test_checksums, is introduced with an extensive
    set of tests covering both online and offline data checksum mode
    changes.  The tests which run concurrent pgbdench during online
    processing are gated behind the PG_TEST_EXTRA flag to some degree
    due to being very time consuming to run.
    
    This work is based on an earlier version of this patch which was
    reviewed by among others Heikki Linnakangas, Robert Haas, Andres
    Freund, Tomas Vondra, Michael Banck and Andrey Borodin.  During
    the work on this new version, Tomas Vondra has given invaluable
    assistance with not only coding and reviewing but very in-depth
    testing.
    
    Author: Daniel Gustafsson <daniel@yesql.se>
    Author: Magnus Hagander <magnus@hagander.net>
    Co-authored-by: Tomas Vondra <tomas@vondra.me>
    Reviewed-by: Tomas Vondra <tomas@vondra.me>
    Reviewed-by: Andres Freund <andres@anarazel.de>
    Reviewed-by: Heikki Linnakangas <hlinnaka@iki.fi>
    Discussion: https://postgr.es/m/CABUevExz9hUUOLnJVr2kpw9Cx=o4MCr1SVKwbupzuxP7ckNutA@mail.gmail.com
    Discussion: https://postgr.es/m/20181030051643.elbxjww5jjgnjaxg@alap3.anarazel.de
    Discussion: https://postgr.es/m/CABUevEwE3urLtwxxqdgd5O2oQz9J717ZzMbh+ziCSa5YLLU_BA@mail.gmail.com

--- src/backend/storage/page/bufpage.c
+++ src/backend/storage/page/bufpage.c
@@ -107,7 +107,15 @@ PageIsVerified(PageData *page, BlockNumber blkno, int flags, bool *checksum_fail
 	 */
 	if (!PageIsNew(page))
 	{
-		if (DataChecksumsEnabled())
+		/*
+		 * There shouldn't be any check for interrupt calls happening in this
+		 * codepath, but just to be on the safe side we hold interrupts since
+		 * if they did happen the data checksum state could change during
+		 * verifying checksums, which could lead to incorrect verification
+		 * results.
+		 */
+		HOLD_INTERRUPTS();
+		if (DataChecksumsNeedVerify())
 		{
 			checksum = pg_checksum_page(page, blkno);
 
@@ -118,6 +126,7 @@ PageIsVerified(PageData *page, BlockNumber blkno, int flags, bool *checksum_fail
 					*checksum_failure_p = true;
 			}
 		}
+		RESUME_INTERRUPTS();
 
 		/*
 		 * The following checks don't prove the header is correct, only that
@@ -1507,9 +1516,14 @@ PageIndexTupleOverwrite(Page page, OffsetNumber offnum,
 void
 PageSetChecksum(Page page, BlockNumber blkno)
 {
+	HOLD_INTERRUPTS();
 	/* If we don't need a checksum, just return */
-	if (PageIsNew(page) || !DataChecksumsEnabled())
+	if (PageIsNew(page) || !DataChecksumsNeedWrite())
+	{
+		RESUME_INTERRUPTS();
 		return;
+	}
 
 	((PageHeader) page)->pd_checksum = pg_checksum_page(page, blkno);
+	RESUME_INTERRUPTS();
 }
